5851425e5e626c8ed5b0ed49078778653be01dab
IgnatYaskevich <78702174+IgnatYaskevich@users.noreply.github.com>
2022-01-08 19:09:14 +0300

    changed names of components.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import {BrowserRouter} from "react-router-dom";
-import {Router} from './routes/Routes';
-
-import './index.css';
-import {MainLayout} from "./components/MainLayout";
-
+import { Router } from "./routes/Routes";
+import "./index.css";
+import { MainLayout } from "./components/MainLayout";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <MainLayout>
-                <Router/>
-            </MainLayout>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <StrictMode>
+    <BrowserRouter>
+      <MainLayout>
+        <Router />
+      </MainLayout>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
 );
